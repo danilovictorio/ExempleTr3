f127595bb5d8fcd9f43564266e31930149b5057c
danilovictorio <a17danvicfer@inspedralbes.cat>
2024-06-09 10:58:32 +0200

    eslint error

--- nuxt/eslint.config.js
+++ nuxt/eslint.config.js
@@ -2,9 +2,11 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginVue from "eslint-plugin-vue";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: { globals: globals.browser },
+    files: ["**/*.{js,jsx,ts,tsx,vue}"], // Aquí especificamos los archivos a lintar
+  },
   pluginJs.configs.recommended,
   ...pluginVue.configs["flat/essential"],
 ];
